07bcae091a9e890c10b308d4b3349e2cea61eb4c
Aleksandr Borisov <nillerprog@gmail.com>
2022-04-19 18:12:05 +0300

    Sorting order component

--- Runtime/BaseWindow.cs
+++ Runtime/BaseWindow.cs
@@ -1,8 +1,14 @@
-﻿namespace UnityGuiManager.Runtime
+﻿using UnityEngine;
+using UnityGuiManager.Runtime.Components;
+
+namespace UnityGuiManager.Runtime
 {
     public abstract class BaseWindow
     {
         private GuiManager _guiManager;
+
+        private RelativeCanvasSortingOrder _sortingOrder;
+        private GameObject _gameObject;
 
         internal GuiLayer Layer { get; private set; }
 
@@ -14,6 +20,17 @@
         internal void Open(GuiLayer layer)
         {
             Layer = layer;
+        }
+
+        internal void SetupGameObject(GameObject gameObject)
+        {
+            _gameObject = gameObject;
+            
+            _sortingOrder = _gameObject.GetComponent<RelativeCanvasSortingOrder>();
+            if (_sortingOrder == null)
+            {
+                _sortingOrder = _gameObject.AddComponent<RelativeCanvasSortingOrder>();
+            }
         }
 
         internal void ChangeLayer(GuiLayer layer)
---
+++ Runtime/Components/RelativeCanvasSortingOrder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace UnityGuiManager.Runtime.Components
+{
+    [RequireComponent(typeof(Canvas))]
+    public class RelativeCanvasSortingOrder : MonoBehaviour
+    {
+        private Canvas _canvas;
+        private Canvas _parentCanvas;
+
+        [SerializeField]
+        private int _sortingOrder;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+            _canvas.overrideSorting = true;
+            _parentCanvas = GetComponentInParent<Canvas>();
+            UpdateSortOrder(_sortingOrder);
+        }
+
+        public void UpdateSortOrder(int sortingOrder)
+        {
+            _sortingOrder = sortingOrder;
+            _canvas.sortingOrder = _parentCanvas.sortingOrder + _sortingOrder;
+        }
+
+        [ContextMenu("Force Update")]
+        private void ForceUpdate()
+        {
+            UpdateSortOrder(_sortingOrder);
+        }
+    }
+}
